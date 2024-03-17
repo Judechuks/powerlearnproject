1f2f328de013e81a3d9ec3006c6c4d026ab9ba51
Jude Chuks <judechuks26@gmail.com>
2024-03-17 15:15:32 +0100

    How to make a list of mixed datatypes

--- Dart with Flutter/Class/Week 2 - Datatypes and Function/lists.dart
+++ Dart with Flutter/Class/Week 2 - Datatypes and Function/lists.dart
@@ -5,4 +5,9 @@ void main() {
   print("Values of names[1] is ${names[1]}"); // index 1
   print("Values of names[2] is ${names[2]}"); // index 2
   print(names);
+  /* NOTE:
+    You can add a list of different datatypes as seen below:
+  */
+  List<dynamic> mixedList = [1, "PowerLearnProject", true, 10.5, "\u{1F600}"];
+  print(mixedList);
 }
