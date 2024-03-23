900ef6c15c25a9863ca754a52285c20fa6e54aa0
Jude Chuks <judechuks26@gmail.com>
2024-03-23 20:08:14 +0100

    Timetable updated

---
+++ Dart with Flutter/Class/Week_5_-_/oop.dart
@@ -0,0 +1,6 @@
+class BankAccount {
+  // properties - can not be null; instead you can add a "?" or "late" to the datatype
+  String? branch_name;
+  late String customer_name;
+  late String balance;
+}
