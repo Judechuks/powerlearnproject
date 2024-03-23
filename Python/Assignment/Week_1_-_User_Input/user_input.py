a8c6f3468dbca3a769fbee0ae1b7b5167cf7e484
Jude Chuks <judechuks26@gmail.com>
2024-03-23 20:07:16 +0100

    Assignment 1 - user input

---
+++ Python/Assignment/Week_1_-_User_Input/user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: ")) 
+location = input("Where do you live?: ")
+print("Hello", name, "you are", age, "years old", "and you live in", location) 
