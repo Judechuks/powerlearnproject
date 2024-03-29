1df7bab3758d19d995b86639a945d9645ff4492a
Jude Chuks <judechuks26@gmail.com>
2024-03-29 15:50:20 +0100

    conditional statement

---
+++ Python/Class/Week 3 - Control Flow/conditional-statement.py
@@ -0,0 +1,27 @@
+num = 20
+
+# if statement
+if num >=20:
+  print(f"The number is {num}")
+
+# if...else statement
+num = 10
+if num >=20:
+  print(f"{num} is greater than or equal to 20")
+else:
+  print(f"{num} is not greater than or equal to 20")
+  
+# if...elif...else Statement
+grade = 73
+if grade >= 80:
+  print("A")
+elif grade >= 70:
+  print("B")
+elif grade >= 60:
+  print("C")
+elif grade >= 50:
+  print("D")
+elif grade >= 40:
+  print("E")
+else:
+  print("F")
