1258006453bc3850bbe4e48931e4c2ac08cdf94b
Jude Chuks <judechuks26@gmail.com>
2024-03-31 19:34:27 +0100

    introduction to oop in python

---
+++ Python/Class/Week_5_-_OOP/oop-intro.py
@@ -0,0 +1,37 @@
+"""
+Object Oriented Programming
+Object-Oriented Programming (OOP) is a programming paradigm in computer science that relies on the concept of classes and objects. It is used to structure a software program into simple, reusable pieces of code blueprints (usually called classes), which are used to create individual instances of objects.
+
+Building blocks of OOP
+Classes - a blueprint of an object
+Objects - an instance of a class
+Methods - methods represent behaviors
+Attributes - information to be stored in a class about an object
+
+Four Principles of OOP
+The four pillars of object-oriented programming are:
+
+1. Inheritance: child classes inherit data and behaviors from the parent class
+2. Encapsulation: containing information in an object, exposing only selected information
+3. Abstraction: only exposing high-level public methods for accessing an object
+4. Polymorphism: many methods can do the same task
+
+What are the benefits of OOP?
+1. Modularity. 
+Encapsulation enables objects to be self-contained, making troubleshooting and collaborative development easier.
+2. Reusability. 
+Code can be reused through inheritance, meaning a team does not have to write the same code multiple times.
+3. Productivity. 
+Programmers can construct new programs quickly through the use of multiple libraries and reusable code.
+
+4. Easily upgradable and scalable. 
+Programmers can implement system functionalities independently.
+
+5. Interface descriptions.
+Descriptions of external systems are simple, due to message-passing techniques that are used for object communication.
+6. Security. 
+Using encapsulation and abstraction, complex code is hidden, software maintenance is easier and internet protocols are protected.
+
+7. Flexibility. 
+Polymorphism enables a single function to adapt to the class it is placed in. Different objects can also pass through the same interface.
+"""
