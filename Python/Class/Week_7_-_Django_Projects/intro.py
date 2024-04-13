9ba994791e2bc0fa93adbdeec4c07ac5eb1f4b64
Jude Chuks <judechuks26@gmail.com>
2024-04-13 15:15:49 +0100

    setup details

---
+++ Python/Class/Week_7_-_Django_Projects/intro.py
@@ -0,0 +1,26 @@
+# To run Django, you need to install python
+
+"""
+1. create a project folder Name
+2. run pip install virtualenv (if it is not already installed)
+3. install python intellisense extension (if it is not already installed)
+4. open the root folder with cmd/powershell
+5. run the code 'virtualenv projectname'
+6. cd foldername
+7. activate the virtualenv
+8. by running 'Scripts\activate'
+9. you can deactivate by running 'deactivate'
+10. run 'pip install django' to install django (this should be done for every project)
+11. run 'django-admin startproject sitename_or_project_name'
+12. run your django project by 'cd sitename_or_project_name'
+13. run 'python manage.py runserver'
+
+To create an app
+14. run 'python manage.py startapp app_name'
+
+templates for your static files - html
+
+Follow the link below to complete setup
+slide link: https://www.canva.com/design/DAF6ODpgCfY/X-CCVymZs6dXgMBYRyCgbg/view
+article on setup: https://melbite.com/melbite/How-to-Get-Started-with-Python-Django
+"""
